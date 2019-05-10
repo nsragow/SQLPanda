9191ad82db4748fc0f7d7229dbc5a0e74f38566d
Noah Sragow <noah@Noahs-MacBook-Air.local>
2019-05-10 10:56:48 -0400

    Now working with pip

--- SQLPanda/SQLPanda.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd, sqlite3
-class SQLDF:
-    def __init__(self,path):
-        self.c = sqlite3.connect(path).cursor()
-    def q(self,query):
-        self.c.execute(query)
-        df = pd.DataFrame(self.c.fetchall())
-        df.columns = [x[0] for x in self.c.description]
-        return df
